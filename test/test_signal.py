d32d398b5771723803b8dff6d03dc627f2fc2c59
nicolaspugnet <85292256+nicolaspugnet@users.noreply.github.com>
2021-07-12 13:25:41 +0200

    Add files via upload

---
+++ test/test_signal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+#Declaration du chemin du fichier a importer
+sys.path.insert(1,'/home/pi/Projet/Full_test/')
+
+#Import de la methode neccessaire
+from test_fonctionne import write_read
+
+#Lancement du test et renvoi dans console du message
+value,duree = write_read('test_signal')
+print(value)
