d4656305a84046f7a918b82eb1327b2e77260d76
nicolaspugnet <85292256+nicolaspugnet@users.noreply.github.com>
2021-07-12 14:41:03 +0200

    Delete library_python.py

--- library_python.py
+++
@@ -1,8 +0,0 @@
-def ecriture_dans_fichier(filename,data):
-    print (filename)
-    # J'ouvre mon fichier "filename" en ecriture
-    logFile = open(filename, "a")
-    # J'ecris "data" dans ce fichier
-    logFile.write(data+chr(10))
-    # Je ferme ce fichier
-    logFile.close
